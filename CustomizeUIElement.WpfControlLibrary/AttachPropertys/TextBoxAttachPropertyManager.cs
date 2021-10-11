1faba0770ef83b7e68d7e4ea9364ab845a7d11bf
joy <1298520866@qq.com>
2021-10-11 23:34:59 +0800

    修改输入框样式

--- CustomizeUIElement.WpfControlLibrary/AttachPropertys/TextBoxAttachPropertyManager.cs
+++ CustomizeUIElement.WpfControlLibrary/AttachPropertys/TextBoxAttachPropertyManager.cs
@@ -22,7 +22,7 @@ namespace CustomizeUIElement.WpfControlLibrary.AttachPropertys
 
         // Using a DependencyProperty as the backing store for TextBoxIcon.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TextBoxIconProperty =
-            DependencyProperty.RegisterAttached("TextBoxIcon", typeof(string), typeof(TextBoxAttachPropertyManager), new PropertyMetadata());
+            DependencyProperty.RegisterAttached("TextBoxIcon", typeof(string), typeof(TextBoxAttachPropertyManager), new PropertyMetadata(string.Empty));
         #endregion
 
         #region placeholder
@@ -38,7 +38,7 @@ namespace CustomizeUIElement.WpfControlLibrary.AttachPropertys
 
         // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty PlaceHolderProperty =
-            DependencyProperty.RegisterAttached("PlaceHolder", typeof(string), typeof(TextBoxAttachPropertyManager), new PropertyMetadata());
+            DependencyProperty.RegisterAttached("PlaceHolder", typeof(string), typeof(TextBoxAttachPropertyManager), new PropertyMetadata(string.Empty));
         #endregion
 
         #region cornerRadius
@@ -55,6 +55,26 @@ namespace CustomizeUIElement.WpfControlLibrary.AttachPropertys
         // Using a DependencyProperty as the backing store for TextBoxCornerRadius.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TextBoxCornerRadiusProperty =
             DependencyProperty.RegisterAttached("TextBoxCornerRadius", typeof(CornerRadius), typeof(TextBoxAttachPropertyManager), new PropertyMetadata(default(CornerRadius)));
+
+        #endregion
+
+        #region 清空按钮
+
+
+        public static bool GetHasClearBtn(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(HasClearBtnProperty);
+        }
+
+        public static void SetHasClearBtn(DependencyObject obj, bool value)
+        {
+            obj.SetValue(HasClearBtnProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HasClearBtn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HasClearBtnProperty =
+            DependencyProperty.RegisterAttached("HasClearBtn", typeof(bool), typeof(TextBoxAttachPropertyManager), new PropertyMetadata(false));
+
 
         #endregion
     }
--- CustomizeUIElement.WpfControlLibrary/Converters/TextBoxIconVisibilityConverter.cs
+++ CustomizeUIElement.WpfControlLibrary/Converters/TextBoxIconVisibilityConverter.cs
@@ -4,15 +4,20 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace CustomizeUIElement.WpfControlLibrary.Converters
 {
+    /// <summary>
+    /// 是否显示Icon的转换器
+    /// </summary>
     public class TextBoxIconVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            string valueStr = value.ToString();
+            return valueStr.Equals(string.Empty) ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -20,4 +25,43 @@ namespace CustomizeUIElement.WpfControlLibrary.Converters
             throw new NotImplementedException();
         }
     }
+
+    /// <summary>
+    /// 是否有清空按钮的转换器
+    /// </summary>
+    public class TextBoxClearBtnVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 是否显示水印的转换器
+    /// </summary>
+    public class TextBoxPlaceHolderVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            //下标0表示的是水印内容
+            //下标1表示的是textbox的内容
+            string content = values[1].ToString();
+            string placeHolder = values[0].ToString();
+            if (content != string.Empty)
+                return Visibility.Collapsed;
+            return placeHolder.Equals(string.Empty) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
